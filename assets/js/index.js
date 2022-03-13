d1ece0f7ed163ba100a0b3ee786c28aa373ba9a9
adam-algatt <adam.algatt@gmail.com>
2022-03-13 14:58:33 -0700

    injected api info into <li>'s in detailed forecast area

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,28 @@
 
 var detailedUi = document.getElementById("detailed-forecast-list");
+var fiveDay = []; 
+
+//function to populate 5 day forecast html elements
+function populateFiveDayForecast(obj) {
+let dailyWeather = obj.daily;
+for (i = 0; i < dailyWeather.length; i++) {
+  let forecast = {}; 
+  let description = dailyWeather[i].weather[0].description;
+  let temp = dailyWeather[i].temp.day + " F";
+  let windSpeed = dailyWeather[i].wind_speed + " mph"; 
+  let humidity = dailyWeather[i].humidity + ' %';
+  Object.assign(forecast, {
+    'description': description,
+    'temperature': temp,
+    'wind': windSpeed,
+   'humidity': humidity,
+  });
+fiveDay.push(forecast);
+forecast = {};console.log();
+};
 
 
+}; 
 function generateDetailedListItem(text) {
     let li = document.createElement('li'); 
     //let append = detailedUl.appendChild('li'); 
@@ -15,9 +36,9 @@ function detailedForecast(obj){
 let li = document.createElement('li'); 
 //let append = detailedUl.appendChild('li'); 
 
-for (var val in obj) {
-    let prop = (`${property}: ${object[property]}`);
-detailedUi.appendChild(generateDetailedListItem(val));
+for (var [key, value] of Object.entries(forecastObj)) {
+    let prop = `${key}: ${value}`;
+detailedUi.appendChild(generateDetailedListItem(prop));
 };
 
 };
@@ -29,15 +50,12 @@ var forecastObj = {};
 
 
 function forecast(data) {
-//     console.log(data);
-//     console.log('temperature:', data.data[0].temp);
-//     console.log('description:', data.data[0].weather.description);
-//     // console.log(data.data[0].temp);
+populateFiveDayForecast(data);
 console.log(data);
     let description = data.current.weather[0].description;
     let temp = data.current.temp + " F";
-    let windSpeed = data.current.wind_speed + "mph"; 
-    let humidity = data.current.humidity + '%';
+    let windSpeed = data.current.wind_speed + " mph"; 
+    let humidity = data.current.humidity + ' %';
     let uvIndex = data.current.uvi; 
     Object.assign(forecastObj, {
       'description': description,
@@ -58,6 +76,6 @@ console.log(data);
 
 
 
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=d32ca2dfefb3211c96c727588cce960f")
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,minutely&appid=d32ca2dfefb3211c96c727588cce960f")
 .then(response => response.json())
    .then(data => forecast(data));
