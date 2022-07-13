510713510f7484872f45d2403a62da13789be2d5
adam-algatt <adam.algatt@gmail.com>
2022-07-12 18:19:59 -0700

    google places api grabs inputted place and returns lat lng so open weather only needs to be called once

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
+// const forecast = require('./Forecast'); 
+const searchForm = document.getElementById('city');
+let input = document.getElementById('autocomplete');
+// let autocomplete = new google.maps.places.Autocomplete(input, {types: ["geocode"]});
+import { weatherData } from "./Weather.js";
+
 
 var detailedUi = document.getElementById("detailed-forecast-list");
 var fiveDay = []; 
@@ -8,37 +14,36 @@ function createFiveDayEls () {
 
 
 //function to populate 5 day forecast html elements
-function populateFiveDayForecast(obj) {
-let dailyWeather = obj.daily;
-for (i = 0; i < dailyWeather.length; i++) {
-  let forecast = {}; 
-  let description = dailyWeather[i].weather[0].description;
-  let temp = dailyWeather[i].temp.day + " F";
-  let windSpeed = dailyWeather[i].wind_speed + " mph"; 
-  let humidity = dailyWeather[i].humidity + ' %';
-  Object.assign(forecast, {
-    'description': description,
-    'temperature': temp,
-    'wind': windSpeed,
-   'humidity': humidity,
+function populateFiveDayForecast(data) {
+
+  let weatherForecast = {}; 
+  let fiveDay = [];
+  let daily = data.daily;
+// map over api data, creating a new object for each day 
+daily.map((el) => {
+  Object.assign(weatherForecast, {
+    'description': el.weather[0].description,
+    'temperature': el.temp.day,
+    'wind': `${el.wind_speed} mph`,
+   'humidity': `${el.humidity}%`,
+   'icon': el.weather[0].icon,
   });
-fiveDay.push(forecast);
+  // console.log(el.humidity, el.weather, 'tits');
+  fiveDay.push(weatherForecast);
+  weatherForecast = {};
+})
+console.log(fiveDay);
+
 // let div = document.createElement('div'); 
 // div.setAttribute('id', 'fiveDayContainer');
-let ul = document.createElement('ul'); 
-ul.setAttribute('id', 'fiveDayList');
-let fiveDayDivs = ['day-one', 'day-two', 'day-three', 'day-four', 'day-five'];
-for (i = 0; )
+// let ul = document.createElement('ul'); 
+// ul.setAttribute('id', 'fiveDayList');
+// let fiveDayDivs = ['day-one', 'day-two', 'day-three', 'day-four', 'day-five'];
 
-let container = document.getElementById('fiveDayContainer');
-container.appendChild('ul');
-
-//empties forecast obj before iterating over the next obj in the 'daily' arr
-forecast = {};
+// let container = document.getElementById('fiveDayContainer');
+// container.appendChild('ul');
 };
 
-
-}; 
 function generateDetailedListItem(text) {
     let li = document.createElement('li'); 
     //let append = detailedUl.appendChild('li'); 
@@ -62,33 +67,47 @@ detailedUi.appendChild(generateDetailedListItem(prop));
 var forecastObj = {};
 
 
-function forecast(data) {
-populateFiveDayForecast(data);
-console.log(data);
-    let description = data.current.weather[0].description;
-    let temp = data.current.temp + " F";
-    let windSpeed = data.current.wind_speed + " mph"; 
-    let humidity = data.current.humidity + ' %';
-    let uvIndex = data.current.uvi; 
-    Object.assign(forecastObj, {
-      'description': description,
-      'temperature': temp,
-      'wind': windSpeed,
-     'humidity': humidity,
-     'UV': uvIndex
-    });
-     console.log(forecastObj);
-    //  injectHtml(forecastObj);
-    detailedForecast(forecastObj);
-    return forecastObj;
-  };
+// function forecast(data) {
+// // populateFiveDayForecast(data);
+// console.log(data);
+//     let description = data.current.weather[0].description;
+//     let temp = data.current.temp + " F";
+//     let windSpeed = data.current.wind_speed + " mph"; 
+//     let humidity = data.current.humidity + ' %';
+//     let uvIndex = data.current.uvi; 
+//     Object.assign(forecastObj, {
+//       'description': description,
+//       'temperature': temp,
+//       'wind': windSpeed,
+//      'humidity': humidity,
+//      'UV': uvIndex
+//     });
+//      console.log(forecastObj);
+//     //  injectHtml(forecastObj);
+//     // detailedForecast(forecastObj);
+//     return forecastObj;
+//   };
 
+// function getWeatherData (url) {
+// fetch(url)
+// .then(response => response.json())
+//    .then(data => console.log(data))//forecast(data));
+// };
 
+// autocomplete.addListener("place_changed", function() {
+//   let placeResult = autocomplete.getPlace();
+//   let name = placeResult.name;
+// let coord = {}; 
+//   // adds geographic coordinates from google autoselect location
+//   let location = placeResult.geometry.location;
+//   //tidying up returned data
+//   location = JSON.stringify({location})
+//   location = JSON.parse(location);
+// //making coords only one decimal place
+// coord.lat = location.location.lat.toFixed(1);
+// coord.lng = location.location.lng.toFixed(1);
+// let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lng}&exclude=current,minutely,hourly,alerts&units=imperial&appid=d32ca2dfefb3211c96c727588cce960f`;
+// getWeatherData(url);
+// });
 
-
-
-
-
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,minutely&appid=d32ca2dfefb3211c96c727588cce960f")
-.then(response => response.json())
-   .then(data => forecast(data));
+populateFiveDayForecast(weatherData);
